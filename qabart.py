66f49efc29af76626b3096c500421fdb7b8865dd
aryehgigi <aryehgigi@gmail.com>
2020-07-07 22:46:15 +0300

    add to subject as well

--- qabart.py
+++ qabart.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pybart import api as bart
-from pattern.en import conjugate
+from pattern.en import conjugate, PAST
 
 
 @st.cache(allow_output_mutation=True)
@@ -47,7 +47,7 @@ def main():
         
         if len(d_subj) != 0 or len(d_obj) != 0:
             for pred_triplet, pred_vals in d_subj.items():
-                st.write('Q: who', 'is' if not pred_triplet[2] else "", pred_triplet[1], '? A: ', ", ".join(pred_vals))
+                st.write('Q: who', 'is' if not pred_triplet[2] else "", conjugate(pred_triplet[1], tense=PAST), '? A: ', ", ".join(pred_vals))
             for pred_triplet, pred_vals in d_obj.items():
                 st.write('Q: who/what', 'was' if pred_triplet[2] else "", conjugate(pred_triplet[1], tense='ppart'), '? A: ', ", ".join(pred_vals))
         else:
